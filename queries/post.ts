21dbaac00a6db10d25ba57864b309e71d8d064ee
Miguelo981 <55580212+Miguelo981@users.noreply.github.com>
2023-06-14 17:49:21 +0200

    Add posts types and model
    - Request interface
    - Response interface

---
+++ queries/post.ts
@@ -0,0 +1,95 @@
+export const postsQuery = `
+query Post ($first: Int, $order: PostsOrder, $featured: Boolean, $postedBefore: DateTime, $postedAfter: DateTime, $topic: String, $after: String, $before: String, $twitterUrl: String, $url: String) {
+    posts(
+        featured: $featured
+        postedBefore: $postedBefore
+        postedAfter: $postedAfter
+        topic: $topic
+        order: $order
+        after: $after
+        first: $first
+        before: $before
+        twitterUrl: $twitterUrl
+        url: $url
+    ) {
+        totalCount
+        edges {
+            cursor
+            node {
+                commentsCount
+                createdAt
+                description
+                featuredAt
+                id
+                isCollected
+                isVoted
+                name
+                reviewsCount
+                reviewsRating
+                slug
+                tagline
+                url
+                userId
+                votesCount
+                website
+                collections {
+                    totalCount
+                }
+                comments {
+                    totalCount
+                }
+                makers {
+                    coverImage
+                    createdAt
+                    headline
+                    id
+                    isFollowing
+                    isMaker
+                    isViewer
+                    name
+                    profileImage
+                    twitterUsername
+                    url
+                    username
+                    websiteUrl
+                }
+                media {
+                    type
+                    url
+                    videoUrl
+                }
+                productLinks {
+                    type
+                    url
+                }
+                thumbnail {
+                    type
+                    url
+                    videoUrl
+                }
+                topics {
+                    totalCount
+                }
+                user {
+                    coverImage
+                    createdAt
+                    headline
+                    id
+                    isFollowing
+                    isMaker
+                    isViewer
+                    name
+                    profileImage
+                    twitterUsername
+                    url
+                    username
+                    websiteUrl
+                }
+                votes {
+                    totalCount
+                }
+            }
+        }
+    }
+}
+`
