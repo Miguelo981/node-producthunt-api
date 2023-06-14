7105279aff79273e261a5a6e171078382d88a05a
Miguelo981 <55580212+Miguelo981@users.noreply.github.com>
2023-06-14 17:49:55 +0200

    Add getposts functions implementations

--- api.ts
+++ api.ts
@@ -1,5 +1,14 @@
-import { BaseAPI } from "./base"
-import { ProductHuntResponse } from "./types"
+import { AxiosInstance, AxiosPromise, AxiosRequestConfig } from 'axios'
+import { API_V, BASE_PATH, BaseAPI, RequestArgs } from './base'
+import {
+  DUMMY_BASE_URL,
+  createRequestFunction,
+  toPathString,
+} from './common'
+import { Configuration } from './configuration'
+import globalAxios from 'axios'
+import { GetPostsRequest, ProductHuntPostResponse } from './models/post'
+import { postsQuery } from './queries/post'
 
 /**
  * ProductHuntAPI - object-oriented interface
@@ -8,8 +17,125 @@ import { ProductHuntResponse } from "./types"
  * @extends {BaseAPI}
  */
 export class ProductHuntAPI extends BaseAPI {
+  /**
+   *
+   * @summary Creates a completion for the provided prompt and parameters.
+   * @param {GetPostsRequest} getPostsRequest
+   * @param {*} [options] Override http request option.
+   * @throws {RequiredError}
+   * @memberof ProductHuntAPI
+   */
+  public getPosts(
+    getPostsRequest?: GetPostsRequest,
+    options?: AxiosRequestConfig
+  ) {
+    return ProductHuntAPIFp(this.configuration)
+      .getPosts(getPostsRequest, options)
+      .then(request => request(this.axios, this.basePath))
+  }
+}
 
-    public getPosts(): ProductHuntResponse {
-        return { name: 'dsdsd' } as any
-    }
+/**
+ * ProductHuntAPI - axios parameter creator
+ * @export
+ */
+export const ProductHuntAPIAxiosParamCreator = function (
+  configuration?: Configuration
+) {
+  return {
+    /**
+     *
+     * @summary Creates a model response for the given chat conversation.
+     * @param {GetPostsRequest} getPostsRequest
+     * @param {*} [options] Override http request option.
+     * @throws {RequiredError}
+     */
+    getPosts: async (
+      getPostsRequest?: GetPostsRequest,
+      options: AxiosRequestConfig = {}
+    ): Promise<RequestArgs> => {
+      // verify required parameter 'getPostsRequest' is not null or undefined
+      //assertParamExists('getPosts', 'getPostsRequest', getPostsRequest)
+      const localVarPath = `${API_V}/api/graphql`
+      const localVarUrlObj = new URL(localVarPath, DUMMY_BASE_URL)
+      let baseOptions
+      if (configuration) {
+        baseOptions = configuration.baseOptions
+      }
+
+      const localVarRequestOptions = {
+        method: 'POST',
+        ...baseOptions,
+        ...options,
+      }
+      const localVarHeaderParameter = {} as any
+      const localVarQueryParameter = {} as any
+
+      localVarHeaderParameter['Content-Type'] = 'application/json'
+
+      const data = {
+        query: getPostsRequest?.query ?? postsQuery,
+        variables: { ...getPostsRequest?.variables },
+      }
+
+      //setVariables(data, getPostsRequest.variables)
+
+      const headersFromBaseOptions =
+        baseOptions && baseOptions.headers ? baseOptions.headers : {}
+      localVarRequestOptions.headers = {
+        ...localVarHeaderParameter,
+        ...headersFromBaseOptions,
+        ...options.headers,
+      }
+      /* localVarRequestOptions.data = serializeDataIfNeeded(
+        getPostsRequest,
+        localVarRequestOptions,
+        configuration
+      ) */
+      localVarRequestOptions.data = data
+
+      return {
+        url: toPathString(localVarUrlObj),
+        options: localVarRequestOptions,
+      }
+    },
+  }
+}
+
+/**
+ * ProductHuntAPI - functional programming interface
+ * @export
+ */
+export const ProductHuntAPIFp = function (configuration?: Configuration) {
+  const localVarAxiosParamCreator =
+    ProductHuntAPIAxiosParamCreator(configuration)
+  return {
+    /**
+     *
+     * @summary Creates a completion for the provided prompt and parameters.
+     * @param {CreateCompletionRequest} createCompletionRequest
+     * @param {*} [options] Override http request option.
+     * @throws {RequiredError}
+     */
+    async getPosts(
+      getPostsRequest?: GetPostsRequest,
+      options?: AxiosRequestConfig
+    ): Promise<
+      (
+        axios?: AxiosInstance,
+        basePath?: string
+      ) => AxiosPromise<ProductHuntPostResponse>
+    > {
+      const localVarAxiosArgs = await localVarAxiosParamCreator.getPosts(
+        getPostsRequest,
+        options
+      )
+      return createRequestFunction(
+        localVarAxiosArgs,
+        globalAxios,
+        BASE_PATH,
+        configuration
+      )
+    },
+  }
 }
--- configuration.ts
+++ configuration.ts
@@ -1,74 +1,151 @@
-import { GrantType } from "./types"
+import { getApiKey } from './common'
+import { GrantType } from './types'
+import globalAxios from 'axios'
 
-const packageJson = require("./package.json");
+//const packageJson = require("./package.json");
 
 export interface ConfigurationParameters {
-    clientId?: string | Promise<string> | ((name: string) => string) | ((name: string) => Promise<string>);
-    clientSecret?: string | Promise<string> | ((name: string) => string) | ((name: string) => Promise<string>);
-    grantType?: GrantType
-    apiKey?: string | Promise<string> | ((name: string) => string) | ((name: string) => Promise<string>);
-    baseOptions?: any;
-    basePath?: string;
+  clientId?:
+    | string
+    | Promise<string>
+    | ((name: string) => string)
+    | ((name: string) => Promise<string>)
+  clientSecret?:
+    | string
+    | Promise<string>
+    | ((name: string) => string)
+    | ((name: string) => Promise<string>)
+  grantType?: GrantType
+  apiKey?:
+    | string
+    | Promise<string>
+    | ((name: string) => string)
+    | ((name: string) => Promise<string>)
+  baseOptions?: any
+  basePath?: string
 }
 
 export class Configuration {
-    /**
-     * Product Hunt app client id
-     * @param name security name
-     * @memberof Configuration
-     */
-    clientId?: string | Promise<string> | ((name: string) => string) | ((name: string) => Promise<string>);
-    /**
-     * 
-     * Product Hunt app client secret
-     * 
-     * @type name security name
-     * @memberof Configuration
-     */
-    clientSecret?: string | Promise<string> | ((name: string) => string) | ((name: string) => Promise<string>);
-    /**
-     * parameter for grant type
-     * 
-     * @type {GrantType}
-     * @memberof Configuration
-     */
-    grantType?: GrantType
-    /**
-     * parameter for grant type
-     * 
-     * @type name security name
-     * @memberof Configuration
-     */
-    apiKey?: string | Promise<string> | ((name: string) => string) | ((name: string) => Promise<string>);
-    /**
-     * base options for axios calls
-     *
-     * @type {any}
-     * @memberof Configuration
-     */
-    baseOptions?: any;
-    /**
-     * override base path
-     *
-     * @type {string}
-     * @memberof Configuration
-     */
-    basePath?: string;
+  /**
+   * Product Hunt app client id
+   * @param name security name
+   * @memberof Configuration
+   */
+  clientId?:
+    | string
+    | Promise<string>
+    | ((name: string) => string)
+    | ((name: string) => Promise<string>)
+  /**
+   *
+   * Product Hunt app client secret
+   *
+   * @type name security name
+   * @memberof Configuration
+   */
+  clientSecret?:
+    | string
+    | Promise<string>
+    | ((name: string) => string)
+    | ((name: string) => Promise<string>)
+  /**
+   * parameter for grant type
+   *
+   * @type {GrantType}
+   * @memberof Configuration
+   */
+  grantType?: GrantType
+  /**
+   * parameter for grant type
+   *
+   * @type name security name
+   * @memberof Configuration
+   */
+  apiKey?:
+    | string
+    | Promise<string>
+    | ((name: string) => string)
+    | ((name: string) => Promise<string>)
+  /**
+   * base options for axios calls
+   *
+   * @type {any}
+   * @memberof Configuration
+   */
+  baseOptions?: any
+  /**
+   * override base path
+   *
+   * @type {string}
+   * @memberof Configuration
+   */
+  basePath?: string
 
-    constructor(param: ConfigurationParameters = {}) {
-        this.clientId = param.clientId
-        this.clientSecret = param.clientSecret
-        this.grantType = param.grantType
-        this.apiKey = param.apiKey
+  constructor(param: ConfigurationParameters = {}) {
+    this.clientId = param.clientId
+    this.clientSecret = param.clientSecret
+    this.grantType = param.grantType
+    this.apiKey = param.apiKey
 
-        if (!this.baseOptions) {
-            this.baseOptions = {};
+    if (!this.baseOptions) {
+      this.baseOptions = {}
+    }
+
+    this.baseOptions.headers = {
+      'User-Agent': `ProductHunt/NodeJS/${/* packageJson.version */ '1.0.4'}`,
+      //Authorization: `Bearer ${this.apiKey}`,
+      ...this.baseOptions.headers,
+    }
+
+    globalAxios.interceptors.request.use(
+      async req => {
+        if (req.url.includes('oauth/token')) return req
+
+        const { headers } = req
+
+        if (headers['Authorization']) return req
+
+        if (!this.apiKey) {
+            const token = await getApiKey(this)
+
+            this.apiKey = token
         }
 
         this.baseOptions.headers = {
-            'User-Agent': `ProductHunt/NodeJS/${packageJson.version}`,
-            'Authorization': `Bearer ${this.apiKey}`,
+            Authorization: `Bearer ${this.apiKey}`,
             ...this.baseOptions.headers,
         }
-    }
+        
+        req.headers['Authorization'] = `Bearer ${this.apiKey}`
+
+        return req
+      },
+      err => {
+        return Promise.reject(err)
+      }
+    )
+  }
+
+  /**
+   * Check if the given MIME is a JSON MIME.
+   * JSON MIME examples:
+   *   application/json
+   *   application/json; charset=UTF8
+   *   APPLICATION/JSON
+   *   application/vnd.company+json
+   * @param mime - MIME (Multipurpose Internet Mail Extensions)
+   * @return True if the given MIME is JSON, false otherwise.
+   */
+  public isJsonMime(mime: string): boolean {
+    // eslint-disable-next-line no-control-regex, no-useless-escape
+    const jsonMime: RegExp = new RegExp(
+      '^(application/json|[^;/ \t]+/[^;/ \t]+[+]json)[ \t]*(;.*)?$',
+      'i'
+    )
+    return (
+      mime !== null &&
+      (jsonMime.test(mime) ||
+        mime.toLowerCase() === 'application/json-patch+json')
+    )
+  }
 }
